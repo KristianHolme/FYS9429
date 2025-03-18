ee140e60aeccfb92d74a44d376443b26088764c4
Kristian Holme <holme.kristian@gmail.com>
2025-03-18 17:56:27 +0100

    modularize

--- RDE_PINNs/_research/data_driven_ode_solver.jl
+++ RDE_PINNs/_research/data_driven_ode_solver.jl
@@ -1,6 +1,7 @@
 using DrWatson
 @quickactivate "RDE_PINNs"
 using DRL_RDE_utils
+using RDEML
 using ProgressMeter
 using NeuralOperators
 using Lux
@@ -16,138 +17,48 @@ using Statistics
 using Base.Threads
 ##
 policies, envs = make_data_policies_and_envs()
+reset_strategies = make_data_reset_strategies()
 ##
-policy = policies[end]
-policy.py_policy.action_space
-policy.py_policy.predict(rand(Float32, 66), deterministic=true)
-policy.py_policy
-##
-n_runs = 16
-run_datas = Vector{Any}(undef, length(policies)*n_runs)
-data = Vector{Tuple{Array{Float32, 3}, Array{Float32, 3}}}(undef, length(policies)*n_runs)
-prog = Progress(n_runs*length(policies), "Collecting data...")
-data_collect_stats = zeros(Int, length(policies), n_runs)
-for i in 1:(length(policies)*n_runs)
-    policy_idx = div(i-1, n_runs) + 1
-    run_idx = mod(i-1, n_runs) + 1
-    policy = policies[policy_idx]
-    env = envs[policy_idx]
-    
-    sim_data = run_policy(policy, env)
-    run_datas[i] = sim_data
-    
-    n_data = length(sim_data.states)
-    raw_data = zeros(Float32, N, 3, n_data)
-    x_data = @view raw_data[:,:,1:end-1]
-    y_data = @view raw_data[:,1:2,2:end]
-    
-    for j in eachindex(sim_data.observations)
-        obs = sim_data.states[j]
-        raw_data[:, 1, j] = obs[1:N]
-        raw_data[:, 2, j] = obs[N+1:2N]
-        raw_data[:, 3, j] .= sim_data.u_ps[j]
-    end
-    data[i] = (x_data, y_data)
-    data_collect_stats[policy_idx, run_idx] += 1
-    next!(prog, showvalues=[("$(typeof(policies[ip]))", sum(data_collect_stats[ip,:])) for ip in eachindex(policies)])
-end
-## inspecting data
-for (i, rdata) in enumerate(run_datas)
-    policy_idx = div(i-1, n_runs) + 1
-    run_idx = mod(i-1, n_runs) + 1
-    fig = plot_shifted_history(rdata, envs[policy_idx].prob.x, title="Run $run_idx, Policy $policy_idx")
-    display(fig)
-end
-## Saving data
-jldsave(datadir("data.jld2"); data)
+n_runs_per_reset_strategy = 4
+run_data, data = collect_data(policies, envs, reset_strategies; n_runs_per_reset_strategy)
+save_data(run_data, data; filename="RDE_sim_data.jld2")
+## Viz
+visualize_data(run_data, policies, envs, reset_strategies, n_runs_per_reset_strategy; save_plots=true)
 ## Loading data
-data = load(datadir("data.jld2"))["data"]
-##
-function train!(model, ps, st, data; lr = 3f-4, epochs=10, losses=[], dev=cpu_device(), AD=AutoZygote())
-    tstate = Training.TrainState(model, ps, st, OptimizationOptimisers.Adam(lr))
-    p = Progress(epochs*length(data))
-    for _ in 1:epochs, (x, y) in dev(data)
-        _, loss, _, tstate = Training.single_train_step!(AD, MSELoss(), (x, y),
-        tstate)
-        push!(losses, loss)
-        next!(p, showvalues=[("Loss", loss)])
-    end
-    return losses
-end
-
-
-# Save the model parameters and state
-# @save "fno_model.jld2" fno ps st
-
+all_data = load(datadir("RDE_sim_data.jld2"))
+run_data = all_data["run_data"]
+data = all_data["data"]
 ## GPU training
 rng = Random.default_rng()
 const cdev = cpu_device()
 const gdev = gpu_device(2)
 # const xdev = reactant_device()
 
-fno = FourierNeuralOperator(gelu; chs=(3, 128, 128, 128, 128, 2), modes=(32,), permuted=Val(true))
+fno_config = FNOConfig()
+fno = FNO(fno_config)
 
 ps, st = Lux.setup(rng, fno) |> gdev;
 
 @time losses = train!(fno, ps, st, data; epochs=100, dev=gdev, lr=0.001f0)
 ##more training
 @time losses = train!(fno, ps, st, data;
-     losses=losses, epochs=100, dev=gdev, lr=3f-5)
+     losses=losses, epochs=40, dev=gdev, lr=3f-4)
 
 ##
-fig = Figure()
-ax = Makie.Axis(fig[1, 1], xlabel="Epoch", ylabel="Loss", xscale=log10, yscale=log10)
-
-# Calculate smoothed line and confidence bands
-window_size = 50
-smoothed = [mean(losses[max(1,i-window_size):i]) for i in 1:length(losses)]
-upper = [quantile(losses[max(1,i-window_size):i], 0.95) for i in 1:length(losses)]
-lower = [quantile(losses[max(1,i-window_size):i], 0.05) for i in 1:length(losses)]
-
-# Plot bands and smoothed line
-band!(ax, 1:length(losses), lower, upper, color=(:blue, 0.2))
-lines!(ax, smoothed, color=:blue, linewidth=2)
-fig
+plot_losses(fno_config, losses)
 ## Test the model
+##
 RDEParams = RDEParam(tmax = 800.0)
 env = RDEEnv(RDEParams,
     dt = 1.0,
     observation_strategy=SectionedStateObservation(minisections=32))
-sim_test_data = run_policy(StepwiseRDEPolicy(env, [20.0f0, 100.0f0, 200.0f0, 300.0f0, 400.0f0, 500.0f0, 600.0f0, 700.0f0], 
-[0.64f0, 0.96f0, 0.45f0, 0.84f0, 0.5f0, 0.75f0, 0.4f0, 0.62f0]), env)
-plot_shifted_history(sim_test_data, env.prob.x)
-test_states = sim_test_data.states[400:end]
-
-test_data = zeros(Float32, N, 3, length(test_states))
-for i in eachindex(test_states)
-    obs = test_states[i]
-    test_data[:, 1, i] = obs[1:N]
-    test_data[:, 2, i] = obs[N+1:2N]
-    test_data[:, 3, i] .= sim_test_data.u_ps[i]
-end
-
-@time output_data, st = Lux.apply(fno, test_data[:,:,1:end-1] |> gdev, ps, st) |> cdev
+policy = StepwiseRDEPolicy(env, [20.0f0, 100.0f0, 200.0f0, 300.0f0, 400.0f0, 500.0f0, 600.0f0, 700.0f0], 
+[0.64f0, 0.96f0, 0.45f0, 0.84f0, 0.5f0, 0.75f0, 0.4f0, 0.62f0])
+## Policy used in training
+policy = policies[end]
+env = envs[end]
 ##
-fig = Figure()
-ax_u = Makie.Axis(fig[1, 1], xlabel="Time", ylabel="u")
-ax_λ = Makie.Axis(fig[2, 1], xlabel="Time", ylabel="λ")
-ls = []
-n_test = length(test_states)
-colors = Makie.wong_colors()[1:3]
-for (i, ind) in enumerate([1, 2, n_test÷2])
-    u_true = lines!(ax_u, test_data[:, 1, ind+1], color=colors[i], linestyle=:dash)
-    u_pred = lines!(ax_u, output_data[:, 1, ind], color=colors[i])
-    λ_true = lines!(ax_λ, test_data[:, 2, ind+1], color=colors[i], linestyle=:dash)
-    λ_pred = lines!(ax_λ, output_data[:, 2, ind], color=colors[i])
-    if ind == 1
-        push!(ls, u_true)
-        push!(ls, u_pred)
-        push!(ls, λ_true)
-        push!(ls, λ_pred)
-    end
-end
-Legend(fig[:,2], ls[1:2], ["True", "Predicted"], vertical=true, tellheight=false)
-fig
+fig = compare_to_policy(;fno, ps, st, policy, env, cdev, gdev)
 ##
 save(plotsdir("fno_test_modes16.png"), fig)
 ##
