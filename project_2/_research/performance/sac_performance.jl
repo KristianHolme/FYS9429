043ddb08eaf4fb88951d50983fa7ad31f6fbbe9b
Kristian Holme <holme.kristian@gmail.com>
2025-08-14 20:48:28 +0200

    investigate runtime-dispatch

--- project_2/_research/performance/sac_performance.jl
+++ project_2/_research/performance/sac_performance.jl
@@ -19,5 +19,21 @@ policy = SACPolicy(observation_space(env), action_space(env);
 agent = SACAgent(policy, alg; verbose=2, log_dir=logdir("sac_performance", "mountainccar_cont"))
 DRiL.TensorBoardLogger.write_hparams!(agent.logger, DRiL.get_hparams(alg), ["env/ep_rew_mean", "train/actor_loss", "train/critic_loss", "train/entropy_loss"])
 ##
-@profview agent, replay_buffer, training_stats = learn!(agent, env, alg, 5_000)
+@profview agent, replay_buffer, training_stats = learn!(agent, env, alg, 1_000)
+##
 @code_warntype learn!(agent, replay_buffer, env, alg, 5_000)
+
+
+
+##
+obs = rand(observation_space(env), 4)
+batched_obs = DRiL.batch(obs, observation_space(env))
+ps = agent.train_state.parameters
+st = agent.train_state.states
+actor_feats, _, st = DRiL.extract_features(policy, batched_obs, ps, st)
+action_means, st = DRiL.get_actions_from_features(policy, actor_feats, ps, st)
+log_std = ps.log_std
+
+batch_dim = ndims(action_means)
+@code_warntype ds = SquashedDiagGaussian.(eachslice(action_means, dims=batch_dim), Ref(log_std))
+##
